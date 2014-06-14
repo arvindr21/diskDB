7661f16ba96bb18c5ba75193707c246195a50427
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-06-14 22:07:39 +0530

    added all.js

---
+++ examples/all.js
@@ -0,0 +1,59 @@
+// once you run npm install diskDB,
+// var db = require('diskdb'); instead of
+var db = require('../lib/diskdb.js');
+
+db.connect('db', ['articles']);
+var article = {
+    title : 'diskDB rocks',
+    published : 'today',
+    rating : '5 stars'
+}
+//save
+var savedArticle = db.articles.save(article);
+console.log(savedArticle);
+
+//findAll
+var foundArticles = db.articles.find();
+console.log(foundArticles);
+
+foundArticles = db.articles.find({rating : '5 stars'});
+console.log(foundArticles);
+
+//findOne
+var foundArticles = db.articles.findOne();
+console.log(foundArticles);
+
+foundArticles = db.articles.findOne({rating : '5 stars'});
+console.log(foundArticles);
+
+//update
+var query = {
+	title : 'diskDB rocks'
+};
+
+var dataToBeUpdate = {
+	title : 'diskDB rocks again!',
+};
+
+var options = {
+	 multi: false,
+	 upsert: false
+};
+
+var updated = db.articles.update(query, dataToBeUpdate, options);
+console.log(updated);
+
+// after update
+foundArticles = db.articles.findOne({rating : '5 stars'});
+console.log(foundArticles);
+
+//count 
+console.log(db.articles.count());
+
+//remove
+db.articles.remove({rating : '5 stars'});
+db.articles.remove();
+
+// db.articles does not exist anymore!
+
+// run : node all.js
