49fb7dd7f10cfb4832bfa1c2c18af5e67b65fb61
Richard Patching <richard@justaddpixels.com>
2016-10-24 16:12:55 +0100

    Fix issue with multiple find not matching all search criteria

--- test/diskdb_test.js
+++ test/diskdb_test.js
@@ -34,6 +34,10 @@ var dbPath = 'test/testdb',
         title: 'diskDB rocks',
         published: 'yesterday'
     },
+    article3 = {
+        title: 'diskDB rocks',
+        published: 'yesterday'
+    },
     //nested objects
     articleComments = {
         title: 'diskDB rocks',
@@ -199,16 +203,17 @@ exports.findAll = {
     },
 
     'findAll : ': function(test) {
-        test.expect(4);
+        test.expect(5);
         //save two record
         diskdb.articles.save(article);
         diskdb.articles.save(article2);
+        diskdb.articles.save(article3);
 
-        test.equal(diskdb.articles.find().length, 2, 'Should find two records');
+        test.equal(diskdb.articles.find().length, 3, 'Should find three records');
         // find with a query
         test.equal(diskdb.articles.find({
             title: 'diskDB rocks'
-        }).length, 2, 'Should find two records with query');
+        }).length, 3, 'Should find three records with query');
         // no record should be returned when the query does not match any records
         test.equal(diskdb.articles.find({
             title: 'dummy text'
@@ -216,8 +221,13 @@ exports.findAll = {
 
         test.equal(diskdb.articles.find({
           title: 'diskDB rocks',
-          published: 'yesterday'
+          published: 'today'
         }).length, 1, 'Should find one record');
+
+        test.equal(diskdb.articles.find({
+          title: 'diskDB rocks',
+          published: 'yesterday'
+        }).length, 2, 'Should find two records');
 
         test.done();
     },
